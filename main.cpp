d0b523aefde0c4cdfc0624d57eed8bb6be39f652
zhouboyang <zhouboyang@chinamobile.com>
2024-07-30 22:55:27 +0800

    insert include

--- main.cpp
+++ main.cpp
@@ -1,1 +1,6 @@
-int main() { std::cout <<"hello world！"; return 0; }
+#include <iostream>  // 添加这一行
+
+int main() {
+    std::cout << "hello world!" << std::endl;  // 修正字符串中的中文感叹号为英文感叹号，并加入 std::endl 以换行
+    return 0;
+}
